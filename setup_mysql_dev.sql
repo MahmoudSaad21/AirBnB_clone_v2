5fada5a77aaf863f4d1eeeaaf95b31d1b9d29e06
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2024-02-21 03:47:30 -0800

    ..

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Grant all privileges on the database to the user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- Grant SELECT privilege on performance_schema database to the user
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
