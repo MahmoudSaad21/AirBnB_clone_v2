aa7a42943c4934047d706b26fa7da1f7992094c5
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2024-02-21 05:38:41 -0800

    ..

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 import os
+import models
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
