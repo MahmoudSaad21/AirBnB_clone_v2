60fc1cccbc6a081ad45063540d79a2de801859f2
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2024-02-21 05:19:44 -0800

    ..

--- models/base_model.py
+++ models/base_model.py
@@ -19,17 +19,24 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
-        if kwargs:
-            for k, v in kwargs.items():
-                if k != '__class__':
-                    setattr(self, k, v)
-            if 'id' not in kwargs:
-                self.id = str(uuid.uuid4())
-            if 'created_at' not in kwargs:
-                self.created_at = self.updated_at = datetime.utcnow()
-        else:
+        if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.utcnow()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+            # if os.getenv('HBNB_TYPE_STORAGE') in ('db'):
+            if not hasattr(kwargs, 'id'):
+                setattr(self, 'id', str(uuid.uuid4()))
+            if not hasattr(kwargs, 'created_at'):
+                setattr(self, 'created_at', datetime.now())
+            if not hasattr(kwargs, 'updated_at'):
+                setattr(self, 'updated_at', datetime.now())
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
