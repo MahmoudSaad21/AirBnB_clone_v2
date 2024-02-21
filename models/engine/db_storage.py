819e611504b357d9da60e3e6142ec68a637a38ad
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2024-02-21 05:54:55 -0800

    ..

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,9 +3,15 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+import urllib.parse
+
 from models.base_model import BaseModel, Base
-import models
-import os
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place, place_amenity
+from models.amenity import Amenity
+from models.review import Review
 
 class DBStorage:
     """This class manages storage of hbnb models in a SQL database"""
@@ -26,21 +32,21 @@ class DBStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        session = self.__session
-        obj_dict = {}
-        if cls:
-            objs = session.query(eval(cls)).all()
-            for obj in objs:
-                key = "{}.{}".format(type(obj).__name__, obj.id)
-                obj_dict[key] = obj
+        objects = dict()
+        all_classes = (User, State, City, Amenity, Place, Review)
+        if cls is None:
+            for class_type in all_classes:
+                query = self.__session.query(class_type)
+                for obj in query.all():
+                    obj_key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+                    objects[obj_key] = obj
         else:
-            for name in models.classes:
-                objs = session.query(models.classes[name]).all()
-                for obj in objs:
-                    key = "{}.{}".format(type(obj).__name__, obj.id)
-                    obj_dict[key] = obj
-        return obj_dict
-
+            query = self.__session.query(cls)
+            for obj in query.all():
+                obj_key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+                objects[obj_key] = obj
+        return objects
+    
     def new(self, obj):
         """Adds new object to storage database"""
         self.__session.add(obj)
