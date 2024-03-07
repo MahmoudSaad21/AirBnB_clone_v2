3e86e042e526bb7cbd9f05253f40f7723eb4f3cf
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2024-03-07 02:43:43 -0800

    ..

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,21 +17,20 @@ def do_deploy(archive_path):
         return False
 
     try:
-        file_name = archive_path.split('/')[-1]
-        folder_name = file_name.split('.')[0]
-        dest = "/tmp/{}".format(file_name)
-
-        put(archive_path, dest)
-        run("mkdir -p /data/web_static/releases/{}/".format(folder_name))
-        run("tar -xzf {} -C /data/web_static/releases/{}/".format(dest, folder_name))
-        run("rm {}".format(dest))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/"
-            .format(folder_name, folder_name))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(folder_name))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(folder_name))
-        print("New version deployed!")
-        return True
+        put(archive_path, '/tmp/')
+        _filename = archive_path.split("/")[-1]
+        filename = _filename.split(".")[0]
+        run('mkdir -p /data/web_static/releases/{}'.format(filename))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format
+            (_filename, filename))
+        run('rm /tmp/{}'.format(_filename))
+        run('mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/'.format(filename, filename))
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(filename))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{} /data/web_static/current'
+            .format(filename))
+        return(True)
     except Exception as e:
         return False
