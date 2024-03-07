515da487244c4c87b9e44342972aeae75089168d
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2024-03-07 03:04:59 -0800

    ..

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,27 +10,38 @@ env.hosts = ['54.167.171.207', '54.172.80.33']
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to your web servers
-    """
-    if not path.exists(archive_path):
+    """Deploys an archive to the servers"""
+    if not os.path.exists(archive_path):
         return False
 
     try:
+        # Upload the archive to /tmp/ directory on the web server
         put(archive_path, '/tmp/')
-        _filename = archive_path.split("/")[-1]
-        filename = _filename.split(".")[0]
-        run('mkdir -p /data/web_static/releases/{}'.format(filename))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format
-            (_filename, filename))
-        run('rm /tmp/{}'.format(_filename))
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'.format(filename, filename))
-        run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(filename))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{} /data/web_static/current'
-            .format(filename))
-        return(True)
+
+        # Extract the archive to /data/web_static/releases/
+        archive_filename = os.path.basename(archive_path)
+        release_dir = '/data/web_static/releases/' + archive_filename[:-4]
+        run('sudo mkdir -p {}'.format(release_dir))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_dir))
+
+        # Remove the uploaded archive
+        run('sudo rm /tmp/{}'.format(archive_filename))
+
+        # Move the extracted files to the correct location
+        run('sudo mv {}/web_static/* {}'.format(release_dir, release_dir))
+
+        # Remove the web_static symbolic link
+        run('sudo rm -rf {}/web_static'.format(release_dir))
+
+        # Delete the current symbolic link
+        run('sudo rm -rf /data/web_static/current')
+
+        # Create a new symbolic link
+        run('ln -s {} /data/web_static/current'.format(release_dir))
+
+        print('New version deployed!')
+        return True
+
     except Exception as e:
+        print('Deployment failed:', e)
         return False
